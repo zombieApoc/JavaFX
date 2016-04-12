2ab019aa4255f7c00a66c473f17fc8942c076299
Michael Grill <Ultramar@michaels-mbp.home>
2016-04-11 21:10:10 -0400

    LotsaWork

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,56 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static java.awt.SystemColor.text;
+
+public class Controller implements Initializable{
+
+    @FXML
+    public ListView list;
+
+    @FXML
+    public TextField name;
+
+    @FXML
+    public TextField phone;
+
+    @FXML
+    public TextField email;
+
+    @FXML
+    public Button add;
+
+    @FXML
+    public Button remove;
+
+
+
+
+    ObservableList<storeContacts> contacts = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL Location, ResourceBundle) {
+        list.setItems(contacts);
+    }
+
+    public void addItem() {
+        contacts.add(new storeContacts(text.getText()));
+        text.setText("");
+    }
+
+    public void removeItem() {
+        storeContacts item = (ToDoItem) list.getSelectionModel().getSelectedItem();
+        contacts.remove(item);
+    }
+
 }
---
+++ src/sample/storeContacts.java
@@ -0,0 +1,19 @@
+package sample;
+
+/**
+ * Created by Ultramar on 4/11/16.
+ */
+public class storeContacts {
+
+    String text = ("%s, %s, %s", name ,phone, email) ;
+
+    public void storeContacts(String text) {
+        this.text = text;
+    }
+
+
+    @Override
+    public String toString() {
+        return text;
+    }
+}
